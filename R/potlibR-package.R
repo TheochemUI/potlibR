94827745c51234b2c2fa408fbce4c644dc6ccbb5
Rohit Goswami <rog32@hi.is>
2023-07-26 02:05:07 +0000

    ENH: Rework the interface in R

--- R/potlibR-package.R
+++ R/potlibR-package.R
@@ -1,5 +1,16 @@
 ## usethis namespace: start
 #' @useDynLib potlibR, .registration = TRUE
 ## usethis namespace: end
-#' @export cuh2pot_list
 NULL
+
+#' @export
+cuh2pot_list <- function(df, hasFixedCol=TRUE) {
+  if (hasFixedCol) {
+    # If 'is_fixed' is present in the dataframe, call the C++ function with a_isFixed = TRUE
+    .Call(`_potlibR_cuh2pot_list`, df, TRUE)
+  } else {
+    # Otherwise, call the C++ function with a_isFixed = FALSE
+    warning("Called without an is_fixed column, forces are not physically relevant")
+    .Call(`_potlibR_cuh2pot_list`, df, FALSE)
+  }
+}
